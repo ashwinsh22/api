4501b85c930aa4480ef6a8a922ddca7d9614c776
ashwinsh22 <137328148+ashwinsh22@users.noreply.github.com>
2023-07-08 17:30:58 +0530

    Create RecordNotFoundException.java

---
+++ RecordNotFoundException.java
@@ -0,0 +1,16 @@
+package com.scb.personalBanking.exception;
+
+public class RecordNotFoundException extends Exception{
+	public RecordNotFoundException() {
+		super();
+	}
+	public RecordNotFoundException(String message, Throwable cause) {
+		super (message, cause);
+	}
+	public RecordNotFoundException(String message) {
+		super(message);
+	}
+	public RecordNotFoundException(Throwable cause) {
+		super(cause);
+	}
+}
