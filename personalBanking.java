ab84ba2ffe01cb575b229058a0fe40bd4047983c
ashwinsh22 <137328148+ashwinsh22@users.noreply.github.com>
2023-07-08 15:04:41 +0530

    commit 3

---
+++ personalBanking.java
@@ -0,0 +1,16 @@
+package com.scb.personalBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class personalBanking {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(personalBanking.class, args);
+	}
+
+}
+
+
